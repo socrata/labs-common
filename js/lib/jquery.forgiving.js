236f16dcbbe309f30499b740b4ca36da1bff1661
Chris Metcalf <chris@chrismetcalf.net>
2015-10-20 19:03:28 -0700

    Adding an $.ajax version of my forgiving wrapper

--- js/lib/jquery.forgiving.js
+++ js/lib/jquery.forgiving.js
@@ -16,5 +16,22 @@
       .then(dfrd.resolve, dfrd.reject);
     return promise;
   };
+
+  $.ajaxForgiving404 = function(settings) {
+    var dfrd = $.Deferred(),
+    promise = dfrd.promise(),
+    jqXHR;
+
+    jqXHR = $.ajax(settings)
+      .fail(function(xhr) {
+        if(xhr.status == 404) {
+          dfrd.resolve(null);
+        } else {
+          dfrd.reject(xhr);
+        }
+      })
+      .then(dfrd.resolve, dfrd.reject);
+    return promise;
+  };
 }(jQuery));
 
