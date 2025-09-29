9133635b7ccc4723ac7fe96968c13e2f1a509241
Michael Chui <michael.chui@socrata.com>
2025-09-29 16:52:11 -0700

    decided to use the lesser HTTP GET format for now

--- js/lib/tryit.js
+++ js/lib/tryit.js
@@ -12,20 +12,7 @@ define(
 
     var matchesV3 = the_href.match(SODA_V3_REGEX);
     var ajaxOptions = matchesV3 ? {
-      url: the_href,
-      method: 'POST',
-      data: JSON.stringify({
-        query: 'select *',
-        page: {
-          pageNumber: 1,
-          pageSize: 10,
-        },
-        includeSynthetic: false,
-      }),
-      headers: {
-        'X-App-Token': 'bHWsGtRFRP9x8Hl8lYivqM1hQ',
-        'Content-Type': 'application/json'
-      },
+      url: the_href.replace('$YOUR_APP_TOKEN', 'bHWsGtRFRP9x8Hl8lYivqM1hQ'),
       dataType: 'text'
     } : {
       url: the_href,
